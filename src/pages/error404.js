0c2f24d1f4aed4a88142f4668b14d6103d0d6dfa
Florian Shkurti <florian@mbp.local>
2025-03-12 17:31:12 -0400

    Fixed 404 reloading issue

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -4,6 +4,19 @@ import { useLocation } from 'react-router-dom';
 
 
 
+
+const Error404 = () => {
+  useEffect(() => {
+    window.location.href = `${process.env.PUBLIC_URL}/404.html`; // Specify the path to your static 404 page
+  }, []);
+
+  return null; // Returns null because we're redirecting
+};
+
+export default Error404;
+
+
+/*
 const Error404 = () => {
   return (
     <iframe
@@ -15,3 +28,4 @@ const Error404 = () => {
 };
 
 export default Error404;
+*/
