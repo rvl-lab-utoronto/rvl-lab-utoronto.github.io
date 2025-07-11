b88bb354bb9e2c9d35a477be793992de8376817c
kimmy190 <aamy.kim@mail.utoronto.ca>
2025-07-11 14:43:12 -0400

    moved gifs to the first page

--- src/data/slideshow.js
+++ src/data/slideshow.js
@@ -1,5 +1,8 @@
 // A list of images
 export const dataSlideshow = [
+    "assets/slideshow/ball_catching_ke.gif",
+    "assets/slideshow/robot-lifting-cup.gif",
+  "assets/slideshow/robot-lifting-fabric.gif",
   "assets/slideshow/husky_utm_2020.jpg",
   "assets/slideshow/arm_farm.jpg",  
   "assets/slideshow/utah_flying_1.jpg",
@@ -7,8 +10,5 @@ export const dataSlideshow = [
   "assets/slideshow/boat_2.jpg",
   "assets/slideshow/visual_search_diver_tracking_iros_2020.jpg",
   "assets/slideshow/maritime-robotics.jpeg",
-  "assets/slideshow/robot-items.jpg",
-  "assets/slideshow/robot-lifting-cup.gif",
-  "assets/slideshow/robot-lifting-fabric.gif",
-  "assets/slideshow/ball_catching_ke.gif"    
+  "assets/slideshow/robot-items.jpg",   
 ]
