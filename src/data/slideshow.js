80b78e20a11232081a5e60a679256b5486186163
Florian Shkurti <florian@mbp-2.local>
2025-09-16 15:50:51 -0700

    Added new boat photo

--- src/data/slideshow.js
+++ src/data/slideshow.js
@@ -1,15 +1,16 @@
 // A list of images
 export const dataSlideshow = [
-    "assets/slideshow/ball_catching_ke.gif",
-    "assets/slideshow/robot-lifting-cup.gif",
-  "assets/slideshow/robot-lifting-fabric.gif",
-  "assets/slideshow/students_robot.jpg",
   "assets/slideshow/husky_utm_2020.jpg",
+
+    "assets/slideshow/ball_catching_ke.gif",
+    //"assets/slideshow/robot-lifting-cup.gif",
+  //"assets/slideshow/robot-lifting-fabric.gif",
+  "assets/slideshow/students_robot.jpg",
   "assets/slideshow/arm_farm.jpg",  
   "assets/slideshow/utah_flying_1.jpg",
   "assets/slideshow/boat_1.jpg",
-  "assets/slideshow/boat_2.jpg",
+  //"assets/slideshow/boat_2.jpg",
   "assets/slideshow/visual_search_diver_tracking_iros_2020.jpg",
-  "assets/slideshow/maritime-robotics.jpeg",
+  "assets/slideshow/maritime_robotics_2.jpg",
   "assets/slideshow/robot-items.jpg",   
 ]
