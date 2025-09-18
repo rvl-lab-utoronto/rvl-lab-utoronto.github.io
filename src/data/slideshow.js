28e13ae23c82d8da48d2302cc68cdc44a307e7f8
kimmy190 <aamy.kim@mail.utoronto.ca>
2025-09-18 13:13:41 -0400

    converted slideshow imgs to webp

--- src/data/slideshow.js
+++ src/data/slideshow.js
@@ -1,16 +1,16 @@
 // A list of images
 export const dataSlideshow = [
-  "assets/slideshow/husky_utm_2020.jpg",
+  "assets/slideshow/webp/husky_utm_2020.webp",
 
-    "assets/slideshow/ball_catching_ke.gif",
+    "assets/slideshow/webp/ball_catching_ke.webp",
     //"assets/slideshow/robot-lifting-cup.gif",
   //"assets/slideshow/robot-lifting-fabric.gif",
-  "assets/slideshow/students_robot.jpg",
-  "assets/slideshow/arm_farm.jpg",  
-  "assets/slideshow/utah_flying_1.jpg",
-  "assets/slideshow/boat_1.jpg",
+  "assets/slideshow/webp/students_robot.webp",
+  "assets/slideshow/webp/arm_farm.webp",  
+  "assets/slideshow/webp/utah_flying_1.webp",
+  "assets/slideshow/webp/boat_1.webp",
   //"assets/slideshow/boat_2.jpg",
-  "assets/slideshow/visual_search_diver_tracking_iros_2020.jpg",
-  "assets/slideshow/maritime_robotics_2.jpg",
-  "assets/slideshow/robot-items.jpg",   
+  "assets/slideshow/webp/visual_search_diver_tracking_iros_2020.webp",
+  "assets/slideshow/webp/maritime_robotics_2.webp",
+  "assets/slideshow/webp/robot-items.webp",   
 ]
